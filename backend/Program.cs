8e3792b32d6e83ec0ae0053079e23de1e333047e
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-11-29 11:13:57 +0100

    Added JWT authentication to service setup in program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,11 @@
-
 using Core.Models;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 namespace backend
 {
@@ -34,6 +37,27 @@ namespace backend
                           .AllowAnyHeader()
                           .AllowAnyMethod();
                 });
+            });
+
+            //JWT
+            var jwtSettings = builder.Configuration.GetSection("Jwt");
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+                };
             });
 
             builder.Services.AddAuthentication();
