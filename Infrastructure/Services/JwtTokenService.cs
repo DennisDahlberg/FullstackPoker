068b4a97b4727d11056c06d0e2257f247659e8cf
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-11-29 11:18:30 +0100

    Added a jwt token service to handle generating tokens once the user logs in

---
+++ Infrastructure/Services/JwtTokenService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class JwtTokenService
+    {
+        private readonly IConfiguration _config;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public JwtTokenService(IConfiguration config, UserManager<IdentityUser> userManager)
+        {
+            _config = config;
+            _userManager = userManager;
+        }
+
+        public async Task<string> CreateTokenAsync(IdentityUser user)
+        {
+            var jwtSettings = _config.GetSection("Jwt");
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var roles = await _userManager.GetRolesAsync(user);
+            authClaims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(jwtSettings["ExpiresInMinutes"])),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
