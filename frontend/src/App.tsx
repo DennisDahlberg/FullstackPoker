88c22835acef0393a4fb285abe6884dd98ae0cbc
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-11-29 14:10:51 +0100

    Added a public route wrapper that checks if the user is already logged in and trying to go to login or register page and sends them back

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import PrivateRoute from "@/components/PrivateRoute";
+import PublicRoute from "@/components/PublicRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -16,8 +17,8 @@ function App() {
       {!hideNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<PrivateRoute><Home/></PrivateRoute>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<PublicRoute><Login/></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><Register/></PublicRoute>} />
         <Route path="*" element={<div className="pt-20 text-center text-4xl">404 - Not Found</div>} />
       </Routes>
     </>
