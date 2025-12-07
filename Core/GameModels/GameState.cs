65c3e42cceaa0fa40719d1497cb54a1628aa9ec5
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-12-07 22:26:59 +0100

    Added gamestate model

---
+++ Core/GameModels/GameState.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.GameModels
+{
+    public enum GameStage
+    {
+        PreFlop,
+        Flop,
+        Turn,
+        River,
+        Showdown
+    }
+
+    public class GameState
+    {
+        public List<Player> Players { get; set; } = [];
+        public List<Card> CommunityCards { get; set; } = [];
+        public int Pot { get; set; }
+        public GameStage Stage { get; set; } = GameStage.PreFlop;
+    }
+}
