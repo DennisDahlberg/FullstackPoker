c18958f4aa3a757aeee76d9398ec9327abe52e7d
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-12-07 22:06:16 +0100

    Added a game controller

---
+++ backend/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class GameController : Controller
+    {
+        [HttpPost]
+        public IActionResult Start()
+        {
+            return Ok();
+        }
+    }
+}
