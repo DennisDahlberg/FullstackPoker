6f61fb31e4aeeed8c91318a7d710526e5b54ed6c
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-12-07 22:24:03 +0100

    Added models for player and card

---
+++ Core/GameModels/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.GameModels
+{
+    public class Card
+    {
+        public string Rank { get; set; } = null!;
+        public string Suit { get; set; } = null!;
+
+        public override string ToString()
+        {
+            return $"{Rank}{Suit}";
+        }
+    }
+}
---
+++ Core/GameModels/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.GameModels
+{
+    public class Player
+    {
+        public string Name { get; set; } = null!;
+        public List<Card> Hand { get; set; } = [];
+        public int Chips { get; set; }
+        public int CurrentBet { get; set; }
+        public bool IsFolded { get; set; }
+        public bool IsPlayer { get; set; }
+    }
+}
