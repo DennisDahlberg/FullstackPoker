c5c19e68c22e499e2a62ee2fb3a562e76fd54342
Dennis Dahlberg <dennisdahlberg02@outlook.com>
2025-12-13 12:33:00 +0100

    Updated password for connectionstring and added automatic migration to program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -104,6 +104,12 @@ namespace backend
 
             app.MapControllers();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
             app.Run();
         }
     }
