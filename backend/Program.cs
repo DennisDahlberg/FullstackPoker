325b7cbc3b3b373da9d63d4d30cdfcf7f8a6f1b9
DennisDahlberg <dennisdalberg02@gmail.com>
2026-01-13 16:01:41 +0100

    Updated program.cs ef core setup to use postgres instead of sql server and updated appsettings with new connection string

--- backend/Program.cs
+++ backend/Program.cs
@@ -34,7 +34,7 @@ namespace backend
 
             //EF Core
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             //Identity
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
