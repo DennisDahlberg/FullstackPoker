9cf9c00d27dfa5648e682301ab85b41f677ebe11
DennisDahlberg <dennisdalberg02@gmail.com>
2026-01-14 11:13:30 +0100

    Updated send friend request endpoint to send singalr message to the user after succeding

--- backend/Controllers/FriendsController.cs
+++ backend/Controllers/FriendsController.cs
@@ -1,10 +1,12 @@
 using Application.Services;
+using backend.Hubs;
 using Core.DTOs.Friend;
 using Core.Models;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace backend.Controllers;
 
@@ -16,12 +18,14 @@ public class FriendsController : Controller
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly UserService _userService;
     private readonly FriendService _friendService;
+    private readonly IHubContext<FriendsHub> _hubContext;
     
-    public FriendsController(UserManager<ApplicationUser> userManager, UserService userService, FriendService friendService)
+    public FriendsController(UserManager<ApplicationUser> userManager, UserService userService, FriendService friendService, IHubContext<FriendsHub> hubContext)
     {
         _userManager = userManager;
         _userService = userService;
         _friendService = friendService;
+        _hubContext = hubContext;
     }
     
     [HttpPost("send")]
@@ -38,6 +42,8 @@ public class FriendsController : Controller
         if (result.IsFailed)
             return BadRequest(result.Errors);
         
+        await _hubContext.Clients.User(targetUser.Id)
+            .SendAsync("ReceiveFriendRequest", currentUser.UserName);
         return Ok();
     }
 }
