29ce9b6579421de0d90ece7b38fbf20ac8e0f047
DennisDahlberg <dennisdalberg02@gmail.com>
2026-02-03 10:58:49 +0100

    Created player game stat model that will show the stats for each player from a game to not over populate the game model

---
+++ Core/Models/Games/PlayerGameStat.cs
@@ -0,0 +1,13 @@
+namespace Core.Models.Games;
+
+public class PlayerGameStat
+{
+    public int Id { get; set; }
+    public int GameId { get; set; }
+    public string UserId { get; set; } = null!;
+    public bool IsWinner { get; set; }
+    public int ChipsStart { get; set; }
+    public int ChipsEnd { get; set; }
+    public int Profit { get; set; }
+    public int HandsWon { get; set; }
+}
