e4955fb1c1784ae28a737db37917948d32c68376
DennisDahlberg <dennisdalberg02@gmail.com>
2026-03-04 11:10:08 +0100

    Added a summary endpoint for statistic controller with null check for logged in user

--- backend/Controllers/StatisticController.cs
+++ backend/Controllers/StatisticController.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,8 +9,20 @@ namespace backend.Controllers;
 [Route("[Controller]")]
 public class StatisticController : Controller
 {
-    public IActionResult GetStatistics()
+    private readonly IUserService _userService;
+
+    public StatisticController(IUserService userService)
     {
+        _userService = userService;
+    }
+
+    [HttpGet("summary")]
+    public async Task<IActionResult> GetSummary()
+    {
+        var user = await _userService.GetLoggedInUser(User);
+        if (user is null)
+            return Unauthorized();
+        
         return Ok();
     }
 }
