9b2073a64d56f2e8b3db46508f81ea29d0b00a5c
DennisDahlberg <dennisdalberg02@gmail.com>
2026-03-12 10:41:49 +0100

    Added dto for stat summary

---
+++ Core/DTOs/Statistics/StatisticSummaryDto.cs
@@ -0,0 +1,12 @@
+namespace Core.DTOs.Statistics;
+
+public class StatisticSummaryDto
+{
+    public int TotalGames { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int WinRate { get; set; }
+    public int TotalProfit { get; set; }
+    public int BiggestWin { get; set; }
+    public int CurrentStreak { get; set; }
+}
