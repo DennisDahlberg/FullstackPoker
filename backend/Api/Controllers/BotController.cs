db9d69e36d8057193fae105bd06212c0852a7e08
DennisDahlberg <dennisdalberg02@gmail.com>
2026-03-26 14:10:05 +0100

    Added dto for create bot

--- backend/Api/Controllers/BotController.cs
+++ backend/Api/Controllers/BotController.cs
@@ -39,4 +39,10 @@ public class BotController : Controller
 
         return Ok(bots);
     }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateBotAsync()
+    {
+        
+    }
 }
---
+++ backend/Core/DTOs/Bot/CreateBotDto.cs
@@ -0,0 +1,11 @@
+using Core.Models;
+
+namespace Core.DTOs.Bot;
+
+public class CreateBotDto
+{
+    public string Username { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string PlayStyle { get; set; } = null!;
+    public SkillLevel SkillLevel { get; set; }
+}
