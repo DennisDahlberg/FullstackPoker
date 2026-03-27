cae5adee0b8b6557bd9406cea417ce491e0783c9
DennisDahlberg <dennisdalberg02@gmail.com>
2026-03-27 13:37:47 +0100

    Added profile image data to create bot dto

--- backend/Core/DTOs/Bot/CreateBotDto.cs
+++ backend/Core/DTOs/Bot/CreateBotDto.cs
@@ -9,4 +9,6 @@ public class CreateBotDto
     public string Description { get; set; } = null!;
     public string PlayStyle { get; set; } = null!;
     public SkillLevel SkillLevel { get; set; }
+    public byte[]? ProfileImageData { get; set; }
+    public string? ImageType { get; set; }
 }
