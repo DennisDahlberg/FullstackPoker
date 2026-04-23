f1dd6c108b90f206d8cf7c39ede559ab737f15af
DennisDahlberg <dennisdalberg02@gmail.com>
2026-04-23 14:39:40 +0200

    gs

--- backend/Core/GameModels/Player.cs
+++ backend/Core/GameModels/Player.cs
@@ -7,6 +7,7 @@
         public int GameStartingChips { get; set; }
         public int RoundStartingChips { get; set; }
         public int StartingRankPoints { get; set; }
+        public int RankProfit { get; set; }
         public int Chips { get; set; }
         public int CurrentBet { get; set; }
         public bool IsFolded { get; set; }
