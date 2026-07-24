0499f0e4dcf1889b710da51e28c99a607d5d0808
DennisDahlberg <dennisdalberg02@gmail.com>
2026-07-24 21:40:55 +0200

    Added localhost:8080 in allowfrontend for CORS when running the application through docker and nginx

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -121,7 +121,7 @@ namespace Api
                 {
                     options.AddPolicy("AllowFrontend", policy =>
                     {
-                        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "https://poker.pokergame.win", "https://pokergame.win")
+                        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:8080", "https://poker.pokergame.win", "https://pokergame.win")
                             .AllowAnyMethod()
                             .AllowAnyHeader()
                             .AllowCredentials();
